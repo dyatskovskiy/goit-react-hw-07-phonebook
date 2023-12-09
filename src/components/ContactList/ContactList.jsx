a72d82791e8beb19fcfc566b26ccdd125dbbde8b
dyatskovskiy <d.yatskovskiy@ukr.net>
2023-12-09 23:27:58 +0200

    set state

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,10 +1,11 @@
 import { Contact } from 'components/Contact/Contact';
 import { Filter } from 'components/Filter/Filter';
 import { useSelector } from 'react-redux';
+import { getContacts, getFilter } from 'redux/selectors';
 
 export const ContactList = () => {
-  const contacts = useSelector(state => state.contacts);
-  const filter = useSelector(state => state.filter);
+  const contacts = useSelector(getContacts);
+  const filter = useSelector(getFilter);
 
   const filteredContacts = contacts.filter(item => {
     return item.name.toLowerCase().includes(filter.toLowerCase());
--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -2,15 +2,26 @@ import { createSlice } from '@reduxjs/toolkit';
 
 const contactsSlice = createSlice({
   name: 'contacts',
-  initialState: [],
+  initialState: {
+    items: [],
+    isLoading: false,
+    error: null,
+  },
   reducers: {
     addContact(state, action) {
-      state.push(action.payload);
+      state.items.push(action.payload);
     },
     deleteContact(state, action) {
-      return state.filter(contact => contact.id !== action.payload);
+      return state.items.filter(contact => contact.id !== action.payload);
+    },
+    setIsLoading(state, action) {
+      return state.isLoading.action.payload;
+    },
+    setError(state, action) {
+      return state.error.action.payload;
     },
   },
 });
 export const contactsReducer = contactsSlice.reducer;
-export const { addContact, deleteContact } = contactsSlice.actions;
+export const { addContact, deleteContact, setIsLoading, setError } =
+  contactsSlice.actions;
